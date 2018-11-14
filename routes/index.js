77876348014a73bde1c99891ff9985720479a3fc
ubuntu <ivovelthoven@hotmail.com>
2018-11-14 22:12:50 +0100

    Added images and index changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IVelthoven' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
